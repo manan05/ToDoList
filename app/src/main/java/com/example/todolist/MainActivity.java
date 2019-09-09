f4cab5166d56bb1ee4138f76864ed5b44c75112d
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-09-10 03:32:57 +0530

    .

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etItem;
+    Button btnAdd;
+    ListView lvItems;
+
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etItem = findViewById(R.id.etItem);
+        btnAdd = findViewById(R.id.btnAdd);
+        lvItems = findViewById(R.id.lvItems);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!etItem.getText().toString().equals("")) {
+                    String data = etItem.getText().toString();
+                    adapter.add(data);
+                    etItem.setText("");
+                    Toast.makeText(MainActivity.this, "Item Added", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
