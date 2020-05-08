841d427b651719e5e709616e078fdac2323e52ed
Manan Arora <31929934+manan0313@users.noreply.github.com>
2020-05-09 04:29:30 +0530

    Added a swipe to delete functionality

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,18 +15,24 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity  {
 
     EditText etItem;
     FloatingActionButton btnAdd;
-    ListView lvItems;
+    SwipeMenuListView lvItems;
     //Created an arrayList of name items
     ArrayList<String> items;
     ArrayAdapter<String> adapter;
+    private static final String TAG = "myApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,14 +62,49 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 }
             }
         });
-        lvItems.setOnItemClickListener(MainActivity.this );
-    }
+//        lvItems.setOnItemClickListener(MainActivity.this);
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        items.remove(position);
-        adapter.notifyDataSetChanged();
-        FileHelper.writeData(items, this);
-        Toast.makeText(this, "Task Deleted", Toast.LENGTH_SHORT).show();
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(170);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_delete);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+
+        };
+        lvItems.setMenuCreator(creator);
+
+        lvItems.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (1) {
+                    case 1:
+                        Log.d(TAG, "onMenuItemClick: Click on delete");
+                        items.remove(position);
+                        adapter.notifyDataSetChanged();
+                        FileHelper.writeData(items, getApplicationContext());
+                        Toast.makeText(MainActivity.this, "Item Deleted", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
+
     }
+    
+
+
 }
